0a7c2b9f034e07e07a44d1d7ee0618665ff50728
Cor Pruijs <cor@pruys.com>
2014-04-03 23:05:44 +0200

    Stuff

--- js/magicbox.js
+++ js/magicbox.js
@@ -1,8 +1,15 @@
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 $(document).ready(function (argument) {
 	for (var i = 100 ; i >= 0; i--) {
 		$(".box").animate({	
 			"top" : "100%",
 			"marginTop" : "-200px",
+
+			"borderRadius" : "+=p5x",
 		} );
 		$(".box").animate({
 			"left" : "100%",
@@ -17,4 +24,38 @@ $(document).ready(function (argument) {
 			"marginLeft" : "0px",
 		});
 	}
+
+
+
+	/* 
+		if (randomAnimation == 1) {
+			$(".box").animate({	
+				"top" : "100%",
+				"marginTop" : "-200px",
+			} );
+		};
+		
+		if (randomAnimation == 2) {
+			$(".box").animate({
+				"left" : "100%",
+				"marginLeft" : "-200px",
+			});
+		}
+		
+		if (randomAnimation == 3) {
+			$(".box").animate({
+				"top" : "0%",
+				"marginTop" : "0px",
+			});
+		}
+		
+		if (randomAnimation == 4) {
+			$(".box").animate({
+				"left" : "0%",
+				"marginLeft" : "0px",
+			});
+		}
+*/
+
+
 })
